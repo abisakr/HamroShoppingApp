af6fa79ceec28514db3a9222f2178af9eccf497f
abisakr <90587873+abisakr@users.noreply.github.com>
2024-07-02 17:03:37 +0545

    change

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HamroShoppingApp.Controllers
 {
-     [Authorize(AuthenticationSchemes = "Bearer")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
     [Route("api/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
