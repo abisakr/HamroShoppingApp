c90ba7c1cb15455d6101b96d41d088e6c08b7a26
abiskar <abiskar.ag@gmail.com>
2026-07-25 23:54:11 +0545

    Frontend Added

--- Frontend/src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import logo from '../assest/NavLogo.png';
-const Logo = ({w,h}) => {
-  return (
-    <img  src={logo} alt="Logo" width={w} height={h}/>
-  )
-}
-
-export default Logo
